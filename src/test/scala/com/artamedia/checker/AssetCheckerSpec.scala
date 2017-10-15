8f963e6ed73fbe63fe37e1f0fd618d56d5666570
Jon Williams <jonscottw@gmail.com>
2017-10-15 10:53:52 +0100

    Enhance test

--- src/test/scala/com/artamedia/checker/AssetCheckerSpec.scala
+++ src/test/scala/com/artamedia/checker/AssetCheckerSpec.scala
@@ -1,6 +1,6 @@
 package com.artamedia.checker
 
-import com.artamedia.checker.domain.{Asset, Check}
+import com.artamedia.checker.domain._
 import com.artamedia.checker.testutil.CheckerGenerators._
 import org.scalatest.FunSpec
 import org.scalatest.Matchers._
@@ -24,5 +24,34 @@ class AssetCheckerSpec extends FunSpec {
         .toBlocking.first().size should ===(0)
     }
 
+    it("should result in no alerts when low check fails") {
+      val sut = new AssetChecker
+
+      val asset = Asset(Symbol("IBM"), Price(100))
+      val check = Check(asset, Price(110), LowCheck)
+
+      sut.runChecks(Observable.just(Set(asset)), Observable.just(Set(check)))
+        .toBlocking.first().size should ===(0)
+    }
+
+    it("should result in no alerts when high check fails") {
+      val sut = new AssetChecker
+
+      val asset = Asset(Symbol("IBM"), Price(110))
+      val check = Check(asset, Price(100), HighCheck)
+
+      sut.runChecks(Observable.just(Set(asset)), Observable.just(Set(check)))
+        .toBlocking.first().size should ===(0)
+    }
+
+    it("should result in one alert when high check succeeds") {
+      val sut = new AssetChecker
+
+      val asset = Asset(Symbol("IBM"), Price(110))
+      val check = Check(asset, Price(120), HighCheck)
+
+      sut.runChecks(Observable.just(Set(asset)), Observable.just(Set(check)))
+        .toBlocking.first().size should ===(1)
+    }
   }
 }
